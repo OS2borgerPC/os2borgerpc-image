20047d60ff0ababcf199b8b419798117a603c2a7
Jørgen Ulrik B. Krag <jubk@magenta-aps.dk>
2013-05-03 11:17:18 +0200

    - Chroot that actually unmounts correctly.

--- image/build/chroot_bibos.sh
+++ image/build/chroot_bibos.sh
@@ -17,18 +17,20 @@ if [ ! -d "$FS_DIR" ]; then
 fi
 
 echo "Mounting device filesystems"
-sudo mount -v --bind /dev "$FS_DIR/dev"
-sudo mount -v --bind /proc "$FS_DIR/proc"
-sudo mount -v --bind /sys "$FS_DIR/sys"
+sudo mount -v -t proc proc "$FS_DIR/proc"
+sudo mount -v -t sysfs sys "$FS_DIR/sys"
+sudo mount -v -o bind /dev "$FS_DIR/dev"
+sudo mount -v -t devpts pts "$FS_DIR/dev/pts/"
 echo "Setting up resolv.conf"
 sudo cp "$FS_DIR/etc/resolv.conf" "$FS_DIR/etc/resolv.conf.chrooted"
 sudo cp /etc/resolv.conf "$FS_DIR/etc/resolv.conf"
 echo "Chroot'ing"
 sudo chroot "$FS_DIR"
 
+sudo umount -v "$FS_DIR/dev/pts/"
 sudo umount -v "$FS_DIR/dev"
-sudo umount -v "$FS_DIR/proc"
 sudo umount -v "$FS_DIR/sys"
+sudo umount -v "$FS_DIR/proc"
 if [ -f "$FS_DIR/etc/resolv.conf.chrooted" ]; then
     sudo mv "$FS_DIR/etc/resolv.conf.chrooted" "$FS_DIR/etc/resolv.conf"
 fi
