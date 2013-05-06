d9ff85df36bf79563dac6fbb214f63cdb65c37fa
Carsten Agger <carstena@magenta-aps.dk>
2013-05-06 15:11:30 +0200

    Included Skype and coded install in finalize script.

--- image/scripts/finalize.sh
+++ image/scripts/finalize.sh
@@ -14,11 +14,41 @@
 
 
 # Printer setup
-sudo system-config-printer
+#sudo system-config-printer
 
 
 # Proprietary stuff
 
-zenity --info --text="Will now install proprietary and restricted systems"
 
-sudo apt-get install ubuntu-restricted-extras
+# 1. Codecs, Adobe Flash, etc.
+
+zenity --question  --text="Installér codecs og proprietære udvidelser, bl.a.  Microsoft fonts?"
+
+if [[  $? -eq 0 ]]
+then 
+    # User pressed "Yes"
+    sudo apt-get install ubuntu-restricted-extras | \
+        tee >(zenity --progress --pulsate)  > /tmp/install_log.txt
+
+
+else
+    zenity --info --text=" Mange film- og lydfiler vil ikke virke ordentligt uden denne udvidelse.
+    Skriv selv 
+
+        sudo apt-get install ubuntu-restricted-extras 
+
+    i en terminal for at installere på et senere tidspunkt."
+    
+fi
+
+
+# 2. Evil, despicable Microsofty Skype
+
+zenity --question --text="Installér Skype?"
+
+if [[ $? -eq 0 ]]
+then
+     sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
+     sudo apt-get update  | zenity --progress --pulsate --text="Opdaterer pakker ..."
+     sudo apt-get install skype  | zenity --progress --pulsate --text="Installerer Skype."
+fi
