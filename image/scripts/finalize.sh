96608d476a8d034afa263ec1367622f1ef32a8f0
Carsten Agger <carstena@magenta-aps.dk>
2013-05-07 12:53:38 +0200

    Finally semi-finalized finalize.

--- image/scripts/finalize.sh
+++ image/scripts/finalize.sh
@@ -48,14 +48,38 @@ zenity --question --text="Installér Skype?"
 
 if [[ $? -eq 0 ]]
 then
+     ATTEMPTED_INSTALL=1
      sudo add-apt-repository "deb http://archive.canonical.com/ $(lsb_release -sc) partner"
      sudo apt-get update  | zenity --progress --pulsate --text="Opdaterer pakker ..."
      sudo apt-get install skype  | zenity --progress --pulsate --text="Installerer Skype."
 fi
 
-if [[ $? -eq 0 ]]
+if [[ ! -z $ATTEMPTED_INSTALL ]]
 then
-    zenity --info --text="Skype er installeret!"
-else
-    zenity --error --text="Skype-installationen mislykkedes! Prøv eventuelt at\
-    installere den manuelt fra Ubuntu Software Center."
+
+    if [[ $? -eq 0 ]]
+    then
+        zenity --info --text="Skype er installeret!"
+    else
+        zenity --error --text="Skype-installationen mislykkedes! Prøv eventuelt at\
+            installere den manuelt fra Ubuntu Software Center."
+    fi
+fi
+
+# 3. Google Chrome (real deal, no Chromium)
+
+zenity --question --text="Installér Google Chrome?"
+
+if [[ $? -eq 0 ]] 
+then
+    # Install it.
+    # Follow procedure described here:
+    # http://www.howopensource.com/2011/10/install-google-chrome-in-ubuntu-11-10-11-04-10-10-10-04/
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+    sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+    sudo apt-get update
+    sudo apt-get install google-chrome-stable
+fi
+
+
+
