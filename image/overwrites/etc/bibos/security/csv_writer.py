0728330cd42331697c181da39ca49e9a6e64ab4d
dals83 <danni@magenta-aps.dk>
2016-05-02 12:47:36 +0200

    security folder added with implementation of usb keyboard added event.

---
+++ image/overwrites/etc/bibos/security/csv_writer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+from datetime import datetime
+# Dont give timestamp as argument
+# csv format: TimeStamp, securityEventCode, Name, PC, Tec sum, Raw data
+
+
+def write_data(data):
+    if(len(data) == 0):
+        return
+
+    line = datetime.now().strftime('%Y%m%d%H%M')
+    for value in data:
+        line += ',' + value
+
+    csvfile = open("/etc/bibos/security/securityevent.csv", "a")
+    csvfile.write(line + '\n')
