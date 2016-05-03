743ce353230562849d343dfb93afc843ec4b40f4
dals83 <danni@magenta-aps.dk>
2016-05-03 13:24:40 +0200

    Updated to copy the new syslog_read instead of syslog_tail.

--- image/overwrites/etc/bibos/security/runmefirst.sh
+++ image/overwrites/etc/bibos/security/runmefirst.sh
@@ -2,6 +2,6 @@
 
 sudo cp 91-detectusbevents.rules /etc/udev/rules.d
 sudo mkdir /etc/bibos/security
-sudo cp csv_writer.py syslog_tail.py keyboardEvent.py /etc/bibos/security
+sudo cp csv_writer.py syslog_read.py keyboardEvent.py /etc/bibos/security
 sudo chmod gu+x /etc/bibos/security/keyboardEvent.py
 
