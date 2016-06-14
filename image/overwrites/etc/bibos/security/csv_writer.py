b29de24fec198800168995bd2c9b02a2d6cc829b
dals83 <danni@magenta-aps.dk>
2016-06-14 13:55:28 +0200

    Moved new line and comma replacement into the csv_writer module.

--- image/overwrites/etc/bibos/security/csv_writer.py
+++ image/overwrites/etc/bibos/security/csv_writer.py
@@ -8,8 +8,10 @@ def write_data(data):
     if(len(data) == 0):
         return
 
-    line = datetime.now().strftime('%Y%m%d%H%M') + ','
-    line += ','.join(data)
+    line = datetime.now().strftime('%Y%m%d%H%M')
+
+    for d in data:
+	line += ',' + d.replace('\n', ' ').replace('\r', '').replace(',', '')
 
     csvfile = open("/etc/bibos/security/securityevent.csv", "a")
     csvfile.write(line + '\n')
--- image/overwrites/etc/bibos/security/keyboardEvent.py
+++ image/overwrites/etc/bibos/security/keyboardEvent.py
@@ -3,13 +3,22 @@
 import sys
 import csv_writer
 import log_read
+from datetime import date, datetime, time, timedelta
 
 # Get lines from syslog
 fname = "/var/log/syslog"
 lines = log_read.read(300, fname)
 
+now = datetime.now()
+
+dt_min = datetime.combine(date.today(), time(7, 40))
+dt_max = datetime.combine(date.today(), time(7, 40)) + timedelta(minutes=25)
+
+if now <= dt_max and now >=  dt_min:
+	        sys.exit()
+
 # Ignore if not a keyboard event
-if(lines.partition('keyboard')[2] == ""):
+if lines.partition('keyboard')[2] == ""	and lines.partition('Keyboard')[2] == "":
         sys.exit()
 
 # securityEventCode, Tec sum, Raw data
@@ -17,16 +26,14 @@ csv_data = []
 # securityEventCode (security problem id)
 csv_data.append("KEYBOARD")
 
-# Ignore first argument
-for i in range(1, len(sys.argv)):
-        # find keyword
-        # select text from keyword until end of line
-        before_keyword, keyword, after_keyword = lines.partition(sys.argv[i])
-        if(after_keyword != ""):
-            splittet_lines = after_keyword.splitlines()
-            if(len(splittet_lines) > 0):
-                # Tec sum
-                csv_data.append("'" + splittet_lines[0] + "'")
+# find keyword
+# select text from keyword until end of line
+before_keyword, keyword, after_keyword = lines.partition('input:')
+if after_keyword != "":
+    splittet_lines = after_keyword.splitlines()
+    if(len(splittet_lines) > 0):
+        # Tec sum
+        csv_data.append("'" + splittet_lines[0] + "'")
 
 lines = lines.replace('\n', ' ').replace('\r', '').replace(',', '')
 
