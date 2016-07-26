33feb1b3a87fa8a9c389d65e01b5f1c6c4e84d6b
dals83 <danni@magenta-aps.dk>
2016-07-26 12:45:53 +0200

    Security scripts updated to dynamically take the problem uid.

--- admin_scripts/security/keyboardEvent.py
+++ admin_scripts/security/keyboardEvent.py
@@ -3,28 +3,33 @@
 import sys
 import csv_writer
 import log_read
-from datetime import date, datetime, time, timedelta
+
+# The estimated boot time, in seconds.
+# This is what you need to change if you wish to adjust the window in which we
+# ignore keyboard events.
+BOOT_TIME=120
+
+
+with open('/proc/uptime', 'r') as f:
+    uptime_seconds = int(f.readline().split('.')[0])
+
+if uptime_seconds <= BOOT_TIME:
+    sys.exit()
 
 # Get lines from syslog
 fname = "/var/log/syslog"
 lines = log_read.read(300, fname)
 
-now = datetime.now()
-
-dt_min = datetime.combine(date.today(), time(7, 40))
-dt_max = datetime.combine(date.today(), time(7, 40)) + timedelta(minutes=25)
-
-if now <= dt_max and now >=  dt_min:
-	        sys.exit()
 
 # Ignore if not a keyboard event
-if lines.partition('keyboard')[2] == ""	and lines.partition('Keyboard')[2] == "":
+if (lines.partition('keyboard')[2] == "" and
+    lines.partition('Keyboard')[2] == ""):
         sys.exit()
 
 # securityEventCode, Tec sum, Raw data
 csv_data = []
 # securityEventCode (security problem id)
-csv_data.append("KEYBOARD")
+csv_data.append("%SECURITY_PROBLEM_UID%")
 
 # find keyword
 # select text from keyword until end of line
