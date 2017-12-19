1acf6b0a6e57a1a81cb65fbf9256b1c05e9881ea
dals83 <danni@magenta.dk>
2017-12-19 15:25:02 +0100

    Modified sed command, so new line and spaces matches the rest of the file.

--- admin_scripts/ubuntu/ubuntu_16.04/chrome-specific/chrome_disable_autologin.sh
+++ admin_scripts/ubuntu/ubuntu_16.04/chrome-specific/chrome_disable_autologin.sh
@@ -18,6 +18,7 @@
 #-
 #================================================================
 #  HISTORY
+#     2017/19/12 : danni : Modified sed command, so new line and spaces matches the rest of the file.
 #     2017/19/12 : danni : Added checks to if file is present, or autologin is already disabled.
 #     2017/19/12 : danni : Script creation.
 #
@@ -31,13 +32,13 @@ if [ ! -f $CHROME_PREFERENCE_FILE  ]
 then
     echo 'File does not exists. Is Google Chrome installed?'
     exit -1
-elif [ grep -Fxq 'credentials_enable_autosignin' $CHROME_PREFERENCE_FILE ]
+elif grep -Fxq 'credentials_enable_autosignin' $CHROME_PREFERENCE_FILE
 then
     echo 'Autologin is already disabled.'
     exit -1
 fi
 
-sed '/countryid_at_install/ a \"credentials_enable_autosignin\":false,\"credentials_enable_service\":false,'
+sed -i '/countryid_at_install/s/$/\n   \"credentials_enable_autosignin\":false,\n    \"credentials_enable_service\":false,/' $CHROME_PREFERENCE_FILE
 
 echo 'Autologin is disabled.'
 
