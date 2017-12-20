1887f993bdee30f86a19b49bcadea017efc7b48b
dals83 <danni@magenta.dk>
2017-12-20 10:55:00 +0100

    Is now checking if gltext is already set or not.

--- admin_scripts/ubuntu/ubuntu_16.04/xscreensaver/change_text_for_xscreensaver.sh
+++ admin_scripts/ubuntu/ubuntu_16.04/xscreensaver/change_text_for_xscreensaver.sh
@@ -11,7 +11,7 @@
 #%
 #================================================================
 #- IMPLEMENTATION
-#-    version         change_text_for_xscreensaver (magenta.dk) 0.0.1
+#-    version         change_text_for_xscreensaver (magenta.dk) 0.1.0
 #-    author          Danni Als
 #-    copyright       Copyright 2017, Magenta Aps"
 #-    license         GNU General Public License
@@ -19,6 +19,8 @@
 #-
 #================================================================
 #  HISTORY
+#     2017/20/12 : danni : Is now checking if gltext is already set or not.
+#     2017/19/12 : danni : Is making this script work.
 #     2017/02/11 : danni : Script creation
 #
 #================================================================
@@ -36,11 +38,25 @@ SCREENSAVER_FILE="/home/.skjult/.xscreensaver"
 TEXT_MODE_LINE='textMode:       literal'
 TEXT_LITERAL='''textLiteral:    '$1''
 
+#   GL: 				gltext -root -no-spin -text "Magenta"	    \n\
 
 if [ -f $SCREENSAVER_FILE ]
 then
-    sed -i "/textMode/c$TEXT_MODE_LINE" $SCREENSAVER_FILE
-    sed -i "/textLiteral/c$TEXT_MODE_LINE" $SCREENSAVER_FILE
+    sed -i "/textMode/$TEXT_MODE_LINE" $SCREENSAVER_FILE
+    # if gtext is present change it.
+    if grep -q 'gltext' $SCREENSAVER_FILE
+    then
+        sed -i "s/\".*\"/\"$1\"/" $SCREENSAVER_FILE
+        echo 'gltext is updated'
+        exit 0
+    else
+        # if not, add it
+        GLTEXT="GL: 				gltext -root -no-spin -text \"$1\"	    \n\\"
+        sed -i "/flipscreen3d -root/ a $GLTEXT" $SCREENSAVER_FILE
+        echo 'New gltext added.'
+        exit 0
+    fi
 else
     echo 'No xscreensaver file present.'
+    exit -1
 fi
