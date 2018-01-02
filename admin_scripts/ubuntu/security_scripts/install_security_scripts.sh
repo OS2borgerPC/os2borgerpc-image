9cb7203b15d97e54b88cd7a526c47b31a2263dfe
dals83 <danni@magenta.dk>
2018-01-02 14:56:40 +0100

    Creates the missing files and folder for the security system to work.

---
+++ admin_scripts/ubuntu/security_scripts/install_security_scripts.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+#================================================================
+# HEADER
+#================================================================
+#% SYNOPSIS
+#+    install_security_scripts.sh
+#%
+#% DESCRIPTION
+#%    This script enables an already running OS2BorgerPC to become
+#%    security surveillance ready. It creates the missing folder and scripts
+#%    and updates the bibos client to the latest version.
+#%
+#================================================================
+#- IMPLEMENTATION
+#-    version         install_security_scripts.sh (magenta.dk) 0.0.1
+#-    author          Danni Als
+#-    copyright       Copyright 2017, Magenta Aps"
+#-    license         GNU General Public License
+#-    email           danni@magenta-aps.dk
+#-
+#================================================================
+#  HISTORY
+#     2017/06/10 : danni : Script creation
+#
+#================================================================
+# END_OF_HEADER
+#================================================================
+
+SECURITY_SCRIPT_HOME=/etc/bibos/security
+
+mkdir -p $SECURITY_SCRIPT_HOME
+
+cd $SECURITY_SCRIPT_HOME
+
+# Get latest files from github.
+wget https://raw.githubusercontent.com/magenta-aps/bibos_image/master/image/overwrites/etc/bibos/security/csv_writer.py
+
+wget https://raw.githubusercontent.com/magenta-aps/bibos_image/master/image/overwrites/etc/bibos/security/log_read.py
+
+ls
+
+pip install --upgrade bibos_client
