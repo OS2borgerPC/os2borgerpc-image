6d03d324e2f2362af185528625f095dfde16e526
andreas-magenta <ann@magenta-aps.dk>
2018-01-23 15:08:28 +0100

    Fixed problem with Amazon logo still showing

--- admin_scripts/ubuntu/ubuntu_16.04/layout/set_default_applications_in_unity_launcher.sh
+++ admin_scripts/ubuntu/ubuntu_16.04/layout/set_default_applications_in_unity_launcher.sh
@@ -48,6 +48,21 @@ else
     su - $AS_USER -s /bin/bash -c 'dbus-launch --exit-with-session /usr/bin/gsettings set com.canonical.Unity.Launcher favorites '\"'['\''application://nautilus-home.desktop'\'', '\''application://firefox.desktop'\'', '\''application://google-chrome.desktop'\'', '\''application://libreoffice-writer.desktop'\'', '\''application://libreoffice-calc.desktop'\'', '\''application://libreoffice-impress.desktop'\'', '\''unity://running-apps'\'', '\''unity://expo-icon'\'', '\''unity://devices'\'']'\"''
 fi
 
+    # Fix problem with Amazon icon still displaying in the launcher
+    AMAZON_DESKTOP_FILE=/home/$AS_USER/.local/share/applications/ubuntu-amazon-default.desktop
+
+    cat <<EOF > $AMAZON_DESKTOP_FILE
+[Desktop Entry]
+Name=Amazon
+Type=Application
+Icon=amazon-store
+Exec=unity-webapps-runner --amazon --app-id=ubuntu-amazon-default
+Hidden=true
+EOF
+    
+    # Give the right permissions
+    chmod 644 $AMAZON_DESKTOP_FILE
+    chown $AS_USER:$AS_USER $AMAZON_DESKTOP_FILE
 
 HIDDEN_DIR=/home/.skjult
 cp /home/user/.config/dconf/$AS_USER $HIDDEN_DIR/.config/dconf/
