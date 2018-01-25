5a59b527e518d536e59be920074367d82020ec53
dals83 <danni@magenta.dk>
2018-01-25 11:45:02 +0100

    This should work, but it does not.

--- admin_scripts/ubuntu/ubuntu_16.04/print-clients/furesoe_princh_install.sh
+++ admin_scripts/ubuntu/ubuntu_16.04/print-clients/furesoe_princh_install.sh
@@ -19,6 +19,8 @@
 #================================================================
 #  HISTORY
 #     2018/19/01 : danni : Script creation
+#     2018/22/01 : danni : Added -y to add-apt and apt-get install.
+#     2018/22/01 : danni : Now checks if HIDDEN _DIR exists.
 #
 #================================================================
 # END_OF_HEADER
@@ -30,16 +32,27 @@ then
     exit -1
 fi
 
-add-apt-repository -y ppa:princh/experimental
+apt-add-repository -y ppa:princh/experimental
 apt-get update
 apt-get install -y princh
 
-HIDDEN_DIR=/home/.skjult
+echo 'Princh is installed.'
 
-ln -s /usr/share/applications/com-princh-print-daemon.desktop $HIDDEN_DIR/.config/autostart/
+HIDDEN_DIR=/home/.skjult/.config/autostart/
+
+if [ ! -d "$HIDDEN_DIR" ];then
+    echo 'Creating dir autostart.'
+    mkdir -p $HIDDEN_DIR
+fi
+
+ln -s /usr/share/applications/com-princh-print-daemon.desktop "$HIDDEN_DIR"
+
+ls "$HIDDEN_DIR"
 
 PRINTER_NAME=$1
 PRINTER_ID=$2
 DESCRIPTION='"'$3'"'
 
-lpadmin -p $PRINTER_NAME -v princh:$PRINTER_ID -D "$DESCRIPTION" -E -P /usr/share/ppd/princh/princh.ppd
+lpadmin -p "$PRINTER_NAME" -v princh:$PRINTER_ID -D "$DESCRIPTION" -E -P /usr/share/ppd/princh/princh.ppd
+
+exit 0
