714f19bf7da84c669063ad9f49fc999595e93a1c
dals83 <danni@magenta.dk>
2018-12-04 12:43:04 +0100

    * now removes gnome software from nautilus menu bar * google-chrome is added to the menubar instead of firefox.

--- admin_scripts/ubuntu/ubuntu_16.04/layout/set_default_applications_in_unity_launcher.sh
+++ admin_scripts/ubuntu/ubuntu_16.04/layout/set_default_applications_in_unity_launcher.sh
@@ -8,14 +8,14 @@
 #% DESCRIPTION
 #%    This script sets the unity launcher to only show:
 #%    * nautilus-home
-#%    * firefox
+#%    * google-chrome
 #%    * libreoffice-writer
 #%    * libreoffice-calc
 #%    * libreoffice-impress
 #%
 #================================================================
 #- IMPLEMENTATION
-#-    version         set_default_applications_in_unity_launcher (magenta.dk) 0.0.2
+#-    version         set_default_applications_in_unity_launcher (magenta.dk) 0.0.3
 #-    author          Danni Als
 #-    copyright       Copyright 2017, Magenta Aps"
 #-    license         GNU General Public License
@@ -26,6 +26,7 @@
 #     2017/22/11 : danni     : Script creation
 #     2017/23/11 : danni     : User Unity Launcher should not contain amazon, ubuntu software and security copy.
 #     2018/07/02 : andreasnn : Remove Amazon and other webapps
+#     2018/04/12 : danni     : Remove gnome Software
 #
 #================================================================
 # END_OF_HEADER
@@ -45,9 +46,9 @@ then
     # Run gsettings with X, change background right away.
     export DISPLAY=:0.0
     export XAUTHORITY=/home/$AS_USER/.Xauthority
-    su - $AS_USER -s /bin/bash -c '/usr/bin/gsettings set com.canonical.Unity.Launcher favorites '\"'['\''application://nautilus-home.desktop'\'', '\''application://firefox.desktop'\'', '\''application://google-chrome.desktop'\'', '\''application://libreoffice-writer.desktop'\'', '\''application://libreoffice-calc.desktop'\'', '\''application://libreoffice-impress.desktop'\'', '\''unity://running-apps'\'', '\''unity://expo-icon'\'', '\''unity://devices'\'']'\"''
+    su - $AS_USER -s /bin/bash -c '/usr/bin/gsettings set com.canonical.Unity.Launcher favorites '\"'['\''application://nautilus-home.desktop'\'', '\''application://google-chrome.desktop'\'', '\''application://libreoffice-writer.desktop'\'', '\''application://libreoffice-calc.desktop'\'', '\''application://libreoffice-impress.desktop'\'', '\''unity://running-apps'\'', '\''unity://expo-icon'\'', '\''unity://devices'\'']'\"''
 else
-    su - $AS_USER -s /bin/bash -c 'dbus-launch --exit-with-session /usr/bin/gsettings set com.canonical.Unity.Launcher favorites '\"'['\''application://nautilus-home.desktop'\'', '\''application://firefox.desktop'\'', '\''application://google-chrome.desktop'\'', '\''application://libreoffice-writer.desktop'\'', '\''application://libreoffice-calc.desktop'\'', '\''application://libreoffice-impress.desktop'\'', '\''unity://running-apps'\'', '\''unity://expo-icon'\'', '\''unity://devices'\'']'\"''
+    su - $AS_USER -s /bin/bash -c 'dbus-launch --exit-with-session /usr/bin/gsettings set com.canonical.Unity.Launcher favorites '\"'['\''application://nautilus-home.desktop'\'', '\''application://google-chrome.desktop'\'', '\''application://libreoffice-writer.desktop'\'', '\''application://libreoffice-calc.desktop'\'', '\''application://libreoffice-impress.desktop'\'', '\''unity://running-apps'\'', '\''unity://expo-icon'\'', '\''unity://devices'\'']'\"''
 fi
 
 cp /home/$AS_USER/.config/dconf/$AS_USER $HIDDEN_DIR/.config/dconf/
@@ -60,3 +61,4 @@ cp /home/$AS_USER/.config/dconf/$AS_USER $HIDDEN_DIR/.config/dconf/
 
 # Removing the package could give problems, depending on the installation method, so:
 mv /usr/share/applications/ubuntu-amazon-default.desktop /usr/share/applications/ubuntu-amazon-default.desktop.org
+mv /usr/share/applications/org.gnome.Software.desktop /usr/share/applications/org.gnome.Software.desktop.org
