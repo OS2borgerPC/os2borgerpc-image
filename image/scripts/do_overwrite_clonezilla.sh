4d7bd97b18b39903be211e794181e84bb5c5caaa
Alexander Faithfull <af@magenta.dk>
2019-02-13 12:46:24 +0100

    Updated the documentation with the new process for making bootable ISOs
    This commit also adds a new script that passes all of the magic options
    to genisoimage

--- image/scripts/do_overwrite_clonezilla.sh
+++ image/scripts/do_overwrite_clonezilla.sh
@@ -12,5 +12,6 @@ if [ -z "$DEST" -o ! -f "$DEST/Clonezilla-Live-Version" ]; then
     fi
 fi
 
-# Now do the deed
-sudo cp -r "$DIR"/../clonezilla-overwrites/* "$DEST"
+# Now do the deed (although don't do it as root unless we have to!)
+cp -r "$DIR"/../clonezilla-overwrites/* "$DEST" || \
+    sudo cp -r "$DIR"/../clonezilla/overwrites/* "$DEST"
