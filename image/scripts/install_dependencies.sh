8b6bb88809349fd08466a53ab99c1bf40faed5d9
dals83 <danni@magent-aps.dk>
2019-11-18 15:06:41 +0100

    install_dependencies.sh uses apt_periodic_control script to setup unattended security updates...

--- image/scripts/install_dependencies.sh
+++ image/scripts/install_dependencies.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+export DEBIAN_FRONTEND=noninteractive
+
 # Find current directory
 
 DIR=$(dirname ${BASH_SOURCE[0]})
 
-# Install BIBOS specific dependencies
+# Install OS2bogerPC specific dependencies
 #           
 # The DEPENDENCIES file contains packages/programs
-# required by BibOS AND extra packages which are free dependencies
+# required by OS2borgerPC AND extra packages which are free dependencies
 # of Skype and MS Fonts - to shorten the postinstall process.
 DEPENDENCIES=( $(cat "$DIR/DEPENDENCIES") )
 
@@ -20,12 +22,12 @@ dpkg -l | grep "^ii" > /tmp/installed-package-list.txt
 grep -w "ii  deja-dup" /tmp/installed-package-list.txt > /dev/null
 if [ $? -eq 0 ]; then
    # Things to get rid of. Factor out to file if many turn up.
-    sudo apt-get -y remove --purge deja-dup
+   apt-get -y remove --purge deja-dup
 fi
 
 # Remove amazon and update notifier
-sudo apt-get -y remvoe --purge --autoremove unity-webapps-*
-sudo apt-get -y remove --purge --autoremove update-notifer
+apt-get -y remove --purge --autoremove unity-webapps-*
+apt-get -y remove --purge --autoremove update-notifer
 
 
 for  package in "${DEPENDENCIES[@]}"
@@ -42,7 +44,7 @@ if [ "$PKGSTOINSTALL" != "" ]; then
     
     # Step 1: Check for valid APT repositories.
 
-    sudo apt-get update &> /dev/null
+    apt-get update &> /dev/null
     RETVAL=$?
     if [ $RETVAL -ne 0 ]; then
         echo "" 1>&2
@@ -56,7 +58,7 @@ if [ "$PKGSTOINSTALL" != "" ]; then
 
     # Step 2: Do the actual installation. Abort if it fails.
     # and install
-    sudo DEBIAN_FRONTEND=noninteractive apt-get -y install $PKGSTOINSTALL | tee /tmp/bibos_install_log.txt
+    apt-get -y install $PKGSTOINSTALL | tee /tmp/bibos_install_log.txt
     RETVAL=$?
     if [ $RETVAL -ne 0 ]; then
         echo "" 1>&2
@@ -67,36 +69,21 @@ if [ "$PKGSTOINSTALL" != "" ]; then
     fi
 
     # upgrade
-    sudo apt-get -y upgrade | tee /tmp/bibos_upgrade_log.txt
-    sudo apt-get -y dist-upgrade | tee /tmp/bibos_upgrade_log.txt
+    apt-get -y upgrade | tee /tmp/bibos_upgrade_log.txt
+    apt-get -y dist-upgrade | tee /tmp/bibos_upgrade_log.txt
 
     # Clean .deb cache to save space
-    sudo apt-get -y autoremove
-    sudo apt-get -y clean
+    apt-get -y autoremove
+    apt-get -y clean
 
 fi
 
 # Install python packages
 pip install --upgrade bibos-utils bibos-client
 
-# Enable security updates to be automatically installed
-CONF="/etc/apt/apt.conf.d/90os2borgerpc-automatic-upgrades"
-
-if ! dpkg -s unattended-upgrades > /dev/null 2>&1; then
-    sudo apt-get -y install unattended-upgrades
-fi
-    cat > "$CONF" <<END
-APT::Periodic::Enable "1";
-APT::Periodic::Unattended-Upgrade "1";
-APT::Periodic::Update-Package-Lists "1";
-#clear Unattended-Upgrade::Allowed-Origins;
-Unattended-Upgrade::Allowed-Origins {
-    "\${distro_id}:\${distro_codename}-security"
-    ; "\${distro_id}ESM:\${distro_codename}"
-};
-
-# We're done!
-END
+# Setup unattended upgrades
+DIR=$(dirname $(dirname $(dirname $(realpath $0 ))))
+"$DIR"/admin-scripts/image_core/apt_periodic_crontrol.sh security
 
 # Install English language package
-sudo apt-get -y install language-pack-en language-pack-gnome-en
+apt-get -y install language-pack-en language-pack-gnome-en
