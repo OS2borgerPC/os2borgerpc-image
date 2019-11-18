a7e8540b9f74559a9c4a61d6b4ddd72f516537fa
dals83 <danni@magent-aps.dk>
2019-11-18 13:32:27 +0100

    Removed unnecessary command...

--- admin_scripts/ubuntu/ubuntu_16.04/xscreensaver/xscreensaver_enable.sh
+++ admin_scripts/ubuntu/ubuntu_16.04/xscreensaver/xscreensaver_enable.sh
@@ -3,8 +3,6 @@
 # Disable Gnome screensaver, install A LOT of graphic screensavers,
 # enable screensaver for user by default.
 
-export DISPLAY=:0.0
-
 apt-get -y remove gnome-screensaver
 apt-get -y install xscreensaver xscreensaver-data xscreensaver-gl
 
