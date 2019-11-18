46ef313a3c035f1e902267ca2918cc7cdcd5e451
dals83 <danni@magent-aps.dk>
2019-11-18 10:57:38 +0100

    Removed header as it is not a magenta supported script yet. It needs to be tested and verified before it can become a script magenta supports.

--- admin_scripts/ubuntu/change_computer_name.sh
+++ admin_scripts/ubuntu/change_computer_name.sh
@@ -1,29 +1,4 @@
 #!/usr/bin/env bash
-#================================================================
-# HEADER
-#================================================================
-#% SYNOPSIS
-#+    ${SCRIPT_NAME} [-hv] [-o[file]] args ...
-#%
-#% DESCRIPTION
-#%    This is a script template
-#%    to start any good shell script.
-#%
-#================================================================
-#- IMPLEMENTATION
-#-    version         ${SCRIPT_NAME} (magenta.dk) 0.0.1
-#-    author          ${NAME}
-#-    copyright       Copyright 2018, Magenta Aps"
-#-    license         GNU General Public License
-#-    email           ${EMAIL}
-#-
-#================================================================
-#  HISTORY
-#     2017/06/10 : danni : Script creation
-#
-#================================================================
-# END_OF_HEADER
-#================================================================
 
 # This script will change the name of the computer, it will not
 # change how it looks in the admin. There will be no validation that this parameter is a valid name.
