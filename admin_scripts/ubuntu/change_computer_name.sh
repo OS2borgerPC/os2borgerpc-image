0e05f4f6a0305feaa40b2b6269df0cc404abde13
dals83 <danni@magent-aps.dk>
2019-11-18 10:26:22 +0100

    As suggested in code review the hostname is also set using hostname(1)

--- admin_scripts/ubuntu/change_computer_name.sh
+++ admin_scripts/ubuntu/change_computer_name.sh
@@ -1,4 +1,29 @@
 #!/usr/bin/env bash
+#================================================================
+# HEADER
+#================================================================
+#% SYNOPSIS
+#+    ${SCRIPT_NAME} [-hv] [-o[file]] args ...
+#%
+#% DESCRIPTION
+#%    This is a script template
+#%    to start any good shell script.
+#%
+#================================================================
+#- IMPLEMENTATION
+#-    version         ${SCRIPT_NAME} (magenta.dk) 0.0.1
+#-    author          ${NAME}
+#-    copyright       Copyright 2018, Magenta Aps"
+#-    license         GNU General Public License
+#-    email           ${EMAIL}
+#-
+#================================================================
+#  HISTORY
+#     2017/06/10 : danni : Script creation
+#
+#================================================================
+# END_OF_HEADER
+#================================================================
 
 # This script will change the name of the computer, it will not
 # change how it looks in the admin. There will be no validation that this parameter is a valid name.
@@ -13,6 +38,7 @@ NEW_NAME=$1
 PREFS_FILE=/etc/hosts
 STARTPAGE_PREFS_LINE="127.0.1.1 $NEW_NAME"
 OLD_LINE=$(grep 127.0.1.1 /etc/hosts)
+hostname $NEW_NAME
 
 # Replace old line with new
 sed -i "s/$OLD_LINE/$STARTPAGE_PREFS_LINE/g" $PREFS_FILE
