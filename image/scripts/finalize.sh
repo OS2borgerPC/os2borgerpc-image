ee61e62bdafbb462525135e0417724925755469e
dals83 <danni@magent-aps.dk>
2019-11-18 15:38:06 +0100

    Removed sudo command as installation process needs to be executed as root from now on.

--- image/scripts/finalize.sh
+++ image/scripts/finalize.sh
@@ -14,9 +14,9 @@ fi
 
 cp script-data/finalize/*.desktop "${XDG_DESKTOP_DIR}/"
 # Copy finalize script to /usr/share/bibos/bin
-sudo cp bibos-postinstall.sh /usr/share/bibos/bin
+cp bibos-postinstall.sh /usr/share/bibos/bin
 # Modify /etc/lightdm/lightdm.conf to avoid automatic user login
-sudo cp /etc/lightdm/lightdm.conf.bibos_firstboot /etc/lightdm/lightdm.conf
-sudo sed -i "s/autologin-user=[a-zA-Z0-9]\+/autologin-user=$USER/" /etc/lightdm/lightdm.conf
+cp /etc/lightdm/lightdm.conf.bibos_firstboot /etc/lightdm/lightdm.conf
+sed -i "s/autologin-user=[a-zA-Z0-9]\+/autologin-user=$USER/" /etc/lightdm/lightdm.conf
 # The PostInstall script should clean up, i.e. reverse all these changes.
 
