0b44a3e6eea760029b28e1900f54771c7c0255b2
dals83 <danni@magent-aps.dk>
2019-11-19 11:17:14 +0100

    Symbolic link is no longer present, so we use realpath instead to reach the finalize script-data.

--- image/scripts/finalize.sh
+++ image/scripts/finalize.sh
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 
-USER=superuser
+DIR=$(dirname $(realpath $0 ))
 
-if [ -f ~/.config/user-dirs.dirs ]; then
-    source ~/.config/user-dirs.dirs
-fi
-if [ "$XDG_DESKTOP_DIR" == "" ]; then
-    XDG_DESKTOP_DIR="$HOME/Skrivebord"
-fi
-
-cp script-data/finalize/*.desktop "${XDG_DESKTOP_DIR}/"
+cp "$DIR/overwrites/usr/share/bibos/script-data/finalize/*.desktop" "/home/superuser/Skrivebord"
 # Copy finalize script to /usr/share/bibos/bin
 cp bibos-postinstall.sh /usr/share/bibos/bin
 # Modify /etc/lightdm/lightdm.conf to avoid automatic user login
 cp /etc/lightdm/lightdm.conf.bibos_firstboot /etc/lightdm/lightdm.conf
-sed -i "s/autologin-user=[a-zA-Z0-9]\+/autologin-user=$USER/" /etc/lightdm/lightdm.conf
+sed -i "s/autologin-user=[a-zA-Z0-9]\+/autologin-user=superuser/" /etc/lightdm/lightdm.conf
 # The PostInstall script should clean up, i.e. reverse all these changes.
-
