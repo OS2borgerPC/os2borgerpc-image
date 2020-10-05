8e9073ccffabf1654a748d745671cf63c3d42215
Carsten Agger <carstena@magenta.dk>
2020-10-05 11:56:29 +0200

    Small refactoring.

--- image/scripts/finalize.sh
+++ image/scripts/finalize.sh
@@ -4,9 +4,6 @@ DIR=$(dirname $(realpath $0 ))
 
 cp "$DIR"/../overwrites/usr/share/os2borgerpc/script-data/finalize/*.desktop "/home/superuser/Skrivebord"
 
-# Copy postinstall script to /usr/share/os2borgerpc/bin
-cp "$DIR/os2borgerpc-postinstall.sh" /usr/share/os2borgerpc/bin
 # Modify /etc/lightdm/lightdm.conf to avoid automatic user login
 cp /etc/lightdm/lightdm.conf.os2borgerpc_firstboot /etc/lightdm/lightdm.conf
-sed -i "s/autologin-user=[a-zA-Z0-9]\+/autologin-user=superuser/" /etc/lightdm/lightdm.conf
-# The PostInstall script should clean up, i.e. reverse all these changes.
+# The PostInstall script should clean up, i.e. reverse the changes to LightDM configuration.
