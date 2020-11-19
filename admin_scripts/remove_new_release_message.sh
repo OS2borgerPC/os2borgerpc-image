854edd5c663fc4d3d2f3e8e7b9e6760e5c7e1a6f
Carsten Agger <carstena@magenta-aps.dk>
2020-11-19 08:52:25 +0100

    Added scripts for setting Chrome as default browser etc.

---
+++ admin_scripts/remove_new_release_message.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+
+release_upgrades_file=/etc/update-manager/release-upgrades
+
+# Simple backup
+if [ ! -f $release_upgrades_file.org ]
+then 
+	cp $release_upgrades_file $release_upgrades_file.org
+fi
+
+# Replace Prompt with never value
+sed -i 's/Prompt=.*/Prompt=never/' $release_upgrades_file
