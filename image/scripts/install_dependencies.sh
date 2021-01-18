f0de23778213230eba406d1c28c6feb485a5ec03
Carsten Agger <carstena@magenta-aps.dk>
2021-01-18 09:01:49 +0100

    Use os2borgerpc-client from PyPI.

--- image/scripts/install_dependencies.sh
+++ image/scripts/install_dependencies.sh
@@ -69,7 +69,7 @@ if [ "$PKGSTOINSTALL" != "" ]; then
 fi
 
 # TODO: Find a more permanent way of handling this.
-pip3 install http://bibos-admin.magenta-aps.dk/archive/client_packages/os2borgerpc_client-0.0.5.1.tar.gz
+pip3 install os2borgerpc-client
 
 # Setup unattended upgrades
 "$DIR/../../admin_scripts/image_core/apt_periodic_control.sh" security
