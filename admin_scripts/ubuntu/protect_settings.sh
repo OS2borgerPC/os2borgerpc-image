6f00b08afb4f8c4e0ed43fde9a4fa07698dbdee1
Carsten Agger <carstena@magenta-aps.dk>
2021-04-07 10:19:24 +0200

    Update with comments from review.

--- admin_scripts/ubuntu/protect_settings.sh
+++ admin_scripts/ubuntu/protect_settings.sh
@@ -4,8 +4,8 @@ set -ex
 
 if [ !  -f "/usr/bin/gnome-control-center.real" ]
 then
-    cp  /usr/bin/gnome-control-center /usr/bin/gnome-control-center.real
-    chown root:superuser superuser /usr/bin/gnome-control-center.real && chmod 770 /usr/bin/gnome-control-center.real
+    dpkg-divert --divert  /usr/bin/gnome-control-center --add /usr/bin/gnome-control-center.real
+    dpkg-statoverride superuser root 770 /usr/bin/gnome-control-center.real
 
 fi
 
