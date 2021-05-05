66c41cc6c8604ce9cc6fcd9a30d2807f426c2807
Carsten Agger <carstena@magenta.dk>
2021-05-05 14:51:08 +0200

    Added script to set the desired options (still untested).

---
+++ admin_scripts/ubuntu/print-clients/set_printer_options.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -ex 
+
+PRINTER=$1
+PAGE_SIZE=$2
+COLOR_MODEL=$3
+DUPLEX=$4
+# TODO:  to specify Landscape vs Portrait?
+
+if [ $PAGE_SIZE != "-" ]
+then
+    lpadmin -p $1 -o PageSize=$PAGE_SIZE
+fi
+
+if [ $COLOR_MODEL != "-" ]
+then
+    lpadmin -p $1 -o ColorModel=$COLOR_MODEL
+fi
+
+if [ $DUPLEX != "-" ]
+then
+    lpadmin -p $1 -o Duplex=$DUPLEX
+fi
+
+
