303b87df47c0922a9e28209eeeee968c1ed9126e
Marcus Funch Mogensen <mfm@magenta.dk>
2021-10-25 13:02:44 +0200

    Set proper permissions on GNOME keyring in do_overwrite

--- image/scripts/do_overwrite.sh
+++ image/scripts/do_overwrite.sh
@@ -18,6 +18,7 @@ cp -r "$DIR"/../overwrites/* $DESTINATION
 
 # Permissions fixup
 chmod 0440 ${DESTINATION}etc/sudoers.d/keep-proxy
+chmod 0400 ${DESTINATION}home/.skjult/.local/share/keyrings/Standardnøglering.keyring
 
 # Remove Bluetooth indicator applet from Borger user
 "$DIR/../../admin_scripts/image_core/remove_bluetooth_applet.sh"
