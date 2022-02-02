be1ee919184a1ea85e08d012fbb3c9e1b09a93d8
Carsten Agger <carstena@magenta.dk>
2022-02-02 11:34:07 +0100

    Update mount handling.

--- image/clonezilla-overwrites/os2borgerpc/install-local.sh
+++ image/clonezilla-overwrites/os2borgerpc/install-local.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd /lib/live/mount/medium
+cd /run/live/medium
 reset
 exec python3 -m os2borgerpc.installer \
-        --image-dir "/lib/live/mount/medium/os2borgerpc-images/os2borgerpc_default" \
+        --image-dir "/run/live/medium/os2borgerpc-images/os2borgerpc_default" \
         "$@"
