567fc808a7b4aaaba12f0235a84e758650fb86d9
Marcus Funch Mogensen <mfm@magenta.dk>
2022-02-04 14:20:52 +0100

    Update apt_periodic_control to latest version from the scripts repo, which also updates google chrome ...which is especially relevant in relation to security updates

--- image/scripts/apt_periodic_control.sh
+++ image/scripts/apt_periodic_control.sh
@@ -39,50 +39,51 @@ export DEBIAN_FRONTEND=noninteractive
 
 CONF="/etc/apt/apt.conf.d/90os2borgerpc-automatic-upgrades"
 
-if [ "$1" != "" -a "$1" != "false" -a "$1" != "falsk" ]; then
-    # Check (quietly) that the unattended-upgrades package is installed, and
-    # install it if it isn't
-    if ! dpkg -s unattended-upgrades > /dev/null 2>&1; then
-        apt-get -y install unattended-upgrades
-    fi
+if [ "$1" != "" ] && [ "$1" != "false" ] && [ "$1" != "falsk" ]; then
+		# Check (quietly) that the unattended-upgrades package is installed, and
+		# install it if it isn't
+		if ! dpkg -s unattended-upgrades > /dev/null 2>&1; then
+			apt-get -y install unattended-upgrades
+		fi
 
-    # Start building the configuration file with two settings, one for
-    # switching unattended upgrades on and one for automatically downloading
-    # updated package indexes
-    cat > "$CONF" <<END
-APT::Periodic::Enable "1";
-APT::Periodic::Unattended-Upgrade "1";
-APT::Periodic::Update-Package-Lists "1";
-END
+		# Start building the configuration file with two settings, one for
+		# switching unattended upgrades on and one for automatically downloading
+		# updated package indexes
+		cat > "$CONF" <<-END
+			APT::Periodic::Enable "1";
+			APT::Periodic::Unattended-Upgrade "1";
+			APT::Periodic::Update-Package-Lists "1";
+		END
 
-    # Now empty the list of allowed origins and start by populating it with
-    # only security-related entries
-    cat >> "$CONF" <<END
-#clear Unattended-Upgrade::Allowed-Origins;
-Unattended-Upgrade::Allowed-Origins {
-    "\${distro_id}:\${distro_codename}-security"
-    ; "\${distro_id}ESM:\${distro_codename}"
-END
+		# Now empty the list of allowed origins and start by populating it with
+		# only security-related entries
+		cat >> "$CONF" <<-END
+			#clear Unattended-Upgrade::Allowed-Origins;
+			Unattended-Upgrade::Allowed-Origins {
+			    "\${distro_id}:\${distro_codename}-security"
+			    ; "\${distro_id}ESM:\${distro_codename}"
+			    ; "Google LLC:stable"
+		END
 
-    # Unless we've been explicitly told we should only add security-related
-    # entries, then also add everything else
-    if [ "$1" != "security" -a "$1" != "sikkerhed" ]; then
-        cat >> "$CONF" <<END
-    ; "\${distro_id}:\${distro_codename}"
-END
-    fi
+		# Unless we've been explicitly told we should only add security-related
+		# entries, then also add everything else
+		if [ "$1" != "security" ] && [ "$1" != "sikkerhed" ]; then
+			cat >> "$CONF" <<-END
+			    ; "\${distro_id}:\${distro_codename}"
+			END
+		fi
 
-    # Finally, close this scope and conclude the configuration file
-    cat >> "$CONF" <<END
-};
-END
+		# Finally, close this scope and conclude the configuration file
+		cat >> "$CONF" <<-END
+			};
+		END
 else
-    # Switch automatic upgrades off entirely
-    cat > "$CONF" <<END
-APT::Periodic::Enable "0";
-APT::Periodic::Unattended-Upgrade "0";
-APT::Periodic::Update-Package-Lists "0";
+		# Switch automatic upgrades off entirely
+		cat > "$CONF" <<-END
+			APT::Periodic::Enable "0";
+			APT::Periodic::Unattended-Upgrade "0";
+			APT::Periodic::Update-Package-Lists "0";
 
-#clear Unattended-Upgrade::Allowed-Origins;
-END
+			#clear Unattended-Upgrade::Allowed-Origins;
+		END
 fi
