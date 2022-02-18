d42f7d559509cb928c598ec4cdb63a9d3b706b86
Carsten Agger <carstena@magenta.dk>
2022-02-18 15:24:43 +0100

    Removed a lot of old Clonezilla-related stuff.

--- image/build/chroot_os2borgerpc.sh
+++ image/build/chroot_os2borgerpc.sh
@@ -9,28 +9,26 @@ fi
 
 DIR=${DIR%/}
 
-FS_DIR="${DIR}/filesystem"
-
-if [ ! -d "$FS_DIR" ]; then
-    echo "$DIR does not exist or does not seem to be a BibOS image"
+if [ ! -d "DIR" ]; then
+    echo "$DIR does not exist"
     exit 1
 fi
 
 echo "Mounting device filesystems"
-sudo mount -v -t proc proc "$FS_DIR/proc"
-sudo mount -v -t sysfs sys "$FS_DIR/sys"
-sudo mount -v -o bind /dev "$FS_DIR/dev"
-sudo mount -v -t devpts pts "$FS_DIR/dev/pts/"
+sudo mount -v -t proc proc "$DIR/proc"
+sudo mount -v -t sysfs sys "$DIR/sys"
+sudo mount -v -o bind /dev "$DIR/dev"
+sudo mount -v -t devpts pts "$DIR/dev/pts/"
 echo "Setting up resolv.conf"
-sudo cp "$FS_DIR/etc/resolv.conf" "$FS_DIR/etc/resolv.conf.chrooted"
-sudo cp /etc/resolv.conf "$FS_DIR/etc/resolv.conf"
+sudo mv "$DIR/etc/resolv.conf" "$DIR/etc/resolv.conf.chrooted"
+sudo cp /etc/resolv.conf "$DIR/etc/resolv.conf"
 echo "Chroot'ing"
-sudo chroot "$FS_DIR"
+sudo chroot "$DIR"
 
-sudo umount -v "$FS_DIR/dev/pts/"
-sudo umount -v "$FS_DIR/dev"
-sudo umount -v "$FS_DIR/sys"
-sudo umount -v "$FS_DIR/proc"
-if [ -f "$FS_DIR/etc/resolv.conf.chrooted" ]; then
-    sudo mv "$FS_DIR/etc/resolv.conf.chrooted" "$FS_DIR/etc/resolv.conf"
+sudo umount -v "$DIR/dev/pts/"
+sudo umount -v "$DIR/dev"
+sudo umount -v "$DIR/sys"
+sudo umount -v "$DIR/proc"
+if [ -f "$DIR/etc/resolv.conf.chrooted" ]; then
+    sudo mv "$DIR/etc/resolv.conf.chrooted" "$DIR/etc/resolv.conf"
 fi
