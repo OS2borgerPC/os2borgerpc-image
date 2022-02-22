053fa44979a6e230bcc9dcbe5e7260d033ad6481
Carsten Agger <carstena@magenta.dk>
2022-02-22 14:36:37 +0100

    Everything now works - TODO: Make it run automagically.

--- image/build/chroot_os2borgerpc.sh
+++ image/build/chroot_os2borgerpc.sh
@@ -15,20 +15,18 @@ if [ ! -d "$DIR" ]; then
 fi
 
 # echo "Setting up resolv.conf"
-# sudo mv "$DIR/etc/resolv.conf" "$DIR/etc/resolv.conf.chrooted"
-# sudo cp /etc/resolv.conf "$DIR/etc/resolv.conf"
+sudo cp /etc/resolv.conf squashfs-root/run/systemd/resolve/stub-resolv.conf
 
 echo "Mounting device filesystems"
 sudo mount -v -t proc none "$DIR/proc"
 sudo mount -v -t sysfs none "$DIR/sys"
-sudo mount -- bind /dev "$DIR/dev"
 sudo mount -v -t devpts none "$DIR/dev/pts/"
 sudo mount --bind /dev/ "$DIR/dev"
 
 echo "Chroot'ing"
 sudo chroot "$DIR"
 
-sudo umount -v "$FS_DIR/dev/pts/"
-sudo umount -v "$FS_DIR/dev"
-sudo umount -v "$FS_DIR/sys"
-sudo umount -v "$FS_DIR/proc"
+sudo umount -v "$DIR/dev/pts/"
+sudo umount -v "$DIR/dev"
+sudo umount -v "$DIR/sys"
+sudo umount -v "$DIR/proc"
