b9058cebe1a7ed59c7397b330b59b2eba02a5bf7
Carsten Agger <carstena@magenta.dk>
2022-02-22 13:16:53 +0100

    Made preseed work, finally.

--- image/build/chroot_os2borgerpc.sh
+++ image/build/chroot_os2borgerpc.sh
@@ -14,18 +14,21 @@ if [ ! -d "$DIR" ]; then
     exit 1
 fi
 
-echo "Setting up resolv.conf"
-sudo mv "$DIR/etc/resolv.conf" "$DIR/etc/resolv.conf.chrooted"
-sudo cp /etc/resolv.conf "$DIR/etc/resolv.conf"
+# echo "Setting up resolv.conf"
+# sudo mv "$DIR/etc/resolv.conf" "$DIR/etc/resolv.conf.chrooted"
+# sudo cp /etc/resolv.conf "$DIR/etc/resolv.conf"
 
 echo "Mounting device filesystems"
+sudo mount -v -t proc none "$DIR/proc"
+sudo mount -v -t sysfs none "$DIR/sys"
+sudo mount -- bind /dev "$DIR/dev"
+sudo mount -v -t devpts none "$DIR/dev/pts/"
 sudo mount --bind /dev/ "$DIR/dev"
 
 echo "Chroot'ing"
 sudo chroot "$DIR"
 
-echo "Mounting proc, sys and pts"
-sudo mount -v -t proc proc "/proc"
-sudo mount -v -t sysfs sys "/sys"
-sudo mount -v -t devpts pts "/dev/pts/"
-
+sudo umount -v "$FS_DIR/dev/pts/"
+sudo umount -v "$FS_DIR/dev"
+sudo umount -v "$FS_DIR/sys"
+sudo umount -v "$FS_DIR/proc"
