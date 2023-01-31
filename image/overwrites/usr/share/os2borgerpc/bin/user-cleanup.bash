8c3209b5cb7d1f2ea790a7e9813d7c9f20605b00
Andreas Poulsen <ap@magenta-aps.dk>
2023-01-31 16:43:26 +0100

    Remove unnecessary spaces in user-cleanup.bash, add apt-get configuration script

--- image/overwrites/usr/share/os2borgerpc/bin/user-cleanup.bash
+++ image/overwrites/usr/share/os2borgerpc/bin/user-cleanup.bash
@@ -5,15 +5,15 @@
 USERNAME="user"
 
 
-	rm --recursive --force /tmp/* /tmp/.??* /home/$USERNAME
-        # Remove pending print jobs
-        PRINTERS=$(lpstat -p | grep printer | awk '{ print $2; }')
+rm --recursive --force /tmp/* /tmp/.??* /home/$USERNAME
+# Remove pending print jobs
+PRINTERS=$(lpstat -p | grep printer | awk '{ print $2; }')
 
-        for PRINTER in $PRINTERS
-        do
-            lprm -P $PRINTER -
-        done
+for PRINTER in $PRINTERS
+do
+    lprm -P $PRINTER -
+done
  
-        # Restore $HOME
-	rsync -vaz /home/.skjult/ /home/$USERNAME/
-	chown -R $USERNAME:$USERNAME /home/$USERNAME
+# Restore $HOME
+rsync -vaz /home/.skjult/ /home/$USERNAME/
+chown -R $USERNAME:$USERNAME /home/$USERNAME
