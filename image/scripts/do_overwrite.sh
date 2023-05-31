54685383cf48cab62f08a3fa799a084edefec959
heini.ovason <heini.ovason@protonmail.com>
2023-05-31 10:33:47 +0200

    Gateway-funktionality removed.

--- image/scripts/do_overwrite.sh
+++ image/scripts/do_overwrite.sh
@@ -14,7 +14,6 @@ DIR=$(dirname $(realpath $0 ))
 cp -r "$DIR"/../overwrites/* $DESTINATION
 
 # Permissions fixup
-chmod 0440 ${DESTINATION}etc/sudoers.d/keep-proxy
 chmod 0400 ${DESTINATION}home/.skjult/.local/share/keyrings/Standardnøglering.keyring
 chown -R superuser:superuser /home/superuser
 
